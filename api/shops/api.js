704ec3cfcac1970f5a9a20c317d7ade353d274ad
z3u2 <ilyassnasr@gmail.com>
2019-06-25 23:22:10 +0100

    Add validation steps to POST flow

--- api/shops/api.js
+++ api/shops/api.js
@@ -1,10 +1,15 @@
 const shopController = require('./controller')
 const express = require('express')
 const { paramIsId } = require('../utils/checkId')
+const { checkBody, cleanBody, checkFields } = require('../utils/fields')
 
 exports.ShopAPI = class {
     constructor() {
-
+        this.checkBody = checkBody
+        this.cleanBody = cleanBody.bind(this)
+        this.checkFields = checkFields.bind(this)
+        this.requiredFields = ['location', 'name']
+        this.fields = ['location', 'name', 'picture']
     }
 
     get router() {
@@ -17,6 +22,9 @@ exports.ShopAPI = class {
         router.get('/:id',paramIsId)
         router.get('/:id', shopController.getItem)
         // POST /
+        router.post('/', this.checkBody)
+        router.post('/', this.checkFields)
+        router.post('/', this.cleanBody)
         router.post('/', shopController.validate)
         router.post('/', shopController.createItem)
         // PUT /:id
