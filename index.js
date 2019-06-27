a95efaa5ea0a5e895cb967cd0e465eef3cbba7df
z3u2 <ilyassnasr@gmail.com>
2019-06-27 03:24:26 +0100

    Add index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+async function start() {
+    const express = require('express'),
+        app = express(),
+        mongoose = require('mongoose')
+
+    if (!process.env.DB_URL) {
+        console.error('Please set DB_URL in .env !')
+        process.exit(1)
+    }
+    await mongoose.connect(
+        process.env.DB_URL,
+        {
+            useNewUrlParser: true,
+            useFindAndModify: false
+        })
+    const { API } = require('./api/api')
+    const api = new API()
+    const router = api.router
+    app.use('/', router)
+    const PORT = process.env.PORT || 3000
+    app.listen(PORT)
+    console.log(`Listening on port : ${PORT}`)
+}
+
+start().then(() => {
+    console.log('Server Started')
+})
