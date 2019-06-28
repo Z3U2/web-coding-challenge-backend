5b5236fb13043f69fbf020ab0b99189d1cb580d7
z3u2 <ilyassnasr@gmail.com>
2019-06-28 11:17:36 +0100

    Add login function

--- test-set/utils.js
+++ test-set/utils.js
@@ -1,3 +1,16 @@
+const express = require('express')
+const app = express()
+const request = require('supertest')
+const {UserAPI} = require('../api/users/api')
+const userAPI = new UserAPI()
+const router = userAPI.router
+app.use('/', router)
+
+const userAccount = {
+    email: "test1@example.com",
+    password: "Hello"
+}
+
 function extractCookies(res) {
     return res.header['set-cookie'].map(cookieString => {
         return cookieString.split('; ').map(cookie => {
@@ -10,4 +23,13 @@ function extractCookies(res) {
     })
 }
 
+async function getLoginCookies() {
+    let res = await request(app)
+        .post('/login')
+        .set('Content-Type', 'application/json')
+        .send(userAccount)
+    return res.header['set-cookie']
+}
+
 module.exports.extractCookies = extractCookies
+module.exports.getLoginCookies = getLoginCookies
